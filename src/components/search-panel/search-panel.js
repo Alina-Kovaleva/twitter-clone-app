0c9e7ccda27f1ce2442089752ebd83244c1f89aa
Alina Kovaleva <a.d.kovaleva@yandex.ru>
2021-01-22 15:46:28 +0100

    Project structure created

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchPanel = () => {
+    return (
+        <input
+            className="search-input form-control"
+            type="text"
+            placeholder='Search by notes'
+        />
+    )
+}
+
+export default SearchPanel;
