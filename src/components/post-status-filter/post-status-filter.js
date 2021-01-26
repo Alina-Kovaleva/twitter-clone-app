9d6dc262e8f1507e35fdbbb9e3146f988fb21592
Alina Kovaleva <a.d.kovaleva@yandex.ru>
2021-01-26 20:04:02 +0100

    Added the ability to filter posts by likes

--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Button } from 'reactstrap';
 import './post-status-filter.css';
 
-const PostStatusFilter = () => {
-    return (
-        <div className='btn-group'>
-            <Button outline color='info'>All</Button>
-            {/* <button type="button" className="btn btn-info">All</button> */}
-            <button type="button" className="btn btn-outline-secondary">By likes</button>
-        </div>
-    )
+export default class PostStatusFilter extends Component {
+    constructor(props) {
+        super(props);
+        this.buttons = [
+            {name: 'all', label: 'All'},
+            {name: 'like', label: 'Liked'}
+        ]
+    }
+    render() {
+        const buttons = this.buttons.map(({name, label}) => {
+            const {filter, onFilterSelect} = this.props;
+            const active = filter === name;
+            const elClass = active ? 'btn-info' : 'btn-outline-secondary';
+            return (
+                <button 
+                    key={name} 
+                    type="button" 
+                    className={`btn ${elClass}`}
+                    onClick={()=> onFilterSelect(name)}
+                    >{label}</button>
+            )
+        });
+        return (
+            <div className='btn-group'>
+                {buttons}
+                {/* <button outline color='info'>All</button> */}
+                {/* <button type="button" className="btn btn-info">All</button> */}
+                {/* <button type="button" className="btn btn-outline-secondary">By likes</button> */}
+            </div>
+        )
+    }
+    
     
 }
-
-export default PostStatusFilter;
