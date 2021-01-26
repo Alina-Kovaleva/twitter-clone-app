0ec70e567d08fe070c7633dae40ebbcd76169a86
Alina Kovaleva <a.d.kovaleva@yandex.ru>
2021-01-26 19:01:51 +0100

    Added the ability to search by posts

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './search-panel.css';
 
-const SearchPanel = () => {
-    return (
-        <input
-            className="search-input form-control"
-            type="text"
-            placeholder='Search by notes'
-        />
-    )
-}
+export default class SearchPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        }
+        this.onUpdateSearch = this.onUpdateSearch.bind(this)
+     }
 
-export default SearchPanel;
+     onUpdateSearch(event) {
+        const term = event.target.value;
+        this.setState({term});
+        this.props.onUpdateSearch(term);
+     }
+
+    render () {
+        return (
+            <input
+                className="search-input form-control"
+                type="text"
+                placeholder='Search by notes'
+                onChange={this.onUpdateSearch}
+            />
+        )
+    }
+    
+}
